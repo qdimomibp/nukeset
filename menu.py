c0d064c79c12ae7afd0d9b9bc9c40f279bb42a25
qdimomibp <qdimomibp@naver.com>
2019-02-18 14:33:06 +0900

    add slate gizmo

--- menu.py
+++ menu.py
@@ -3,4 +3,5 @@ import nuke
 tb = nuke.toolbar("Nodes")
 m = tb.addMenu("smoke", icon="one_eyed_prince.png")
 m.addMenu("Draw")
+m.addCommand("Draw/slate", "nuke.createNode('slate')")
 m.addCommand("Draw/timecode_burnin", "nuke.createNode('timecode_burnin')")
